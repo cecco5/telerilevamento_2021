40b94f9949ab5d6dd41470920846bd95a377bbca
cecco5 <iudica.francesco@gmail.com>
2021-07-10 12:19:53 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -11,3 +11,13 @@ setwd("C:/lab")
 
 #import
 lst <- raster("c_gls_LST_202107100000_GLOBE_GEO_V2.0.1.nc") #land surface temperature global
+
+#abbiamo un singolo strato quindi il colore lo decidiamo noi con una colorRampPalette
+
+cl <- colorRampPalette(c("light blue","green","red","yellow")) (100)
+plot(lst,col=cl)
+
+#funzione aggregate -> ricampionamento dell'immagine per alleggerirne il peso. args fact = 10 -> 10x10 = 100 pixels per fare la media
+lst_agg <- aggregate(lst,fact=50) #immagine RESAMPLED = RICAMPIONATA
+lst_agg
+plot(lst_agg,col=cl)
