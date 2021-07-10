cba977bf556557b6ecf34911aaa7126bf4fdc979
cecco5 <iudica.francesco@gmail.com>
2021-07-10 13:34:08 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -5,4 +5,8 @@ setwd("C:/lab")
 #lib
 #install.packages("knitr")
 library(knitr)
-stitch("code.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))    #stitch: primo arg -> nome del file con lo script, secondo arg -> template per il report
+stitch("rcode_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))    #stitch: primo arg -> nome del file con lo script, secondo arg -> template per il report
+
+#la funzione prende in input uno script salvato sulla wd e genera in output due file di report:
+# file.tex
+# file.pdf
