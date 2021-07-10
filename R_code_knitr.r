6c602a20290d9ea28a91aa5888ff14898a6d5a7f
cecco5 <iudica.francesco@gmail.com>
2021-07-10 12:47:35 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -5,3 +5,4 @@ setwd("C:/lab")
 #lib
 #install.packages("knitr")
 library(knitr)
+stitch("code.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))    #stitch: primo arg -> nome del file con lo script, secondo arg -> template per il report
