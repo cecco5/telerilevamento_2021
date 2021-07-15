dbd8bd9805051acbe2b97d6178d1c562f2e56359
cecco5 <iudica.francesco@gmail.com>
2021-07-15 14:50:50 +0200

    Create R_code_variability.r

---
+++ R_code_variability.r
@@ -0,0 +1,9 @@
+#R_code_variability.r
+
+library(raster)
+library(RStoolbox)
+# install.packages("RStoolbox")
+library(ggplot2) # for ggplot plotting
+library(gridExtra) # for plotting ggplots together
+# install.packages("viridis")
+library(viridis) # for ggplot colouring
