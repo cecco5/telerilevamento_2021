a9bdad7aea8c6de319921a094c6d3d59373301d0
cecco5 <iudica.francesco@gmail.com>
2021-07-23 02:24:49 +0200

    Update R_code_exam_project.r

--- R_code_exam_project.r
+++ R_code_exam_project.r
@@ -1,1 +1,17 @@
+#R_code_exam_project.r
 
+#------------------------------------------------------------
+#PROGETTO ESAME FINALE TELERILEVAMENTO GEOECOLOGICO
+
+# Summary
+#1  Scaricamento immagini satellitari, riferite a due periodi diversi nel tempo : Copernicus - Sentinel 2
+#2  
+
+#---------------------------------------
+#1 DOWNLOAD
+
+setwd("C:/lab/esame")
+library(raster)
+
+image <- brick("E000N60_PROBAV_LC100_global_v3.0.1_2015-base_Discrete-Classification-map_EPSG-4326.tif")
+plot(image)
