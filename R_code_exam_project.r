71a2df62e3152632631e361b14064a35bba2a931
cecco5 <iudica.francesco@gmail.com>
2021-07-27 16:08:37 +0200

    Update R_code_exam_project.r

--- R_code_exam_project.r
+++ R_code_exam_project.r
@@ -10,13 +10,16 @@
 #4  NDVI analisi multitemporale
 
 
-#---------------------------------------
+#----------------------------------------------------------------
  
 
 setwd("C:/lab/esame")
 
 library(raster)
-libraty(RStoolbox) #per la classificazione
+library(RStoolbox) #per la classificazione
+library(ggplot2)
+library(gridExtra)
+library(viridis)
 
 
 
@@ -36,6 +39,7 @@ libraty(RStoolbox) #per la classificazione
 list2014 <- list.files(pattern="20140806")
 s2014 <- stack(list2014)                                        #raster multibanda 2014, oggetto RasterStack
 #plotRGB(s2014,4,3,2,stretch="hist") funziona
+
 
 #2015
 list2015 <- list.files(pattern="20150910")
@@ -64,4 +68,39 @@ s2020 <- stack(list2020)
 #2021
 list2021 <- list.files(pattern="20210302")
 s2021 <- stack(list2021)
+# plotRGB(s2021, 4,3,2, stretch="hist")
+
+#plot 2014 and 2021 images
+
+
+#------------------------------------------------------------------------------------#
+
+# 2.1 unsupervised classification with unsuperClass function
+
+# unsuperClass classification based on kmeans algorithm
+# Per cogliere il massimo della variabilità dai nostri dati operiamo una PCA rispettivamente su set 2014 e 2021
+pca_2014 <- rasterPCA(s2014)
+
+#> summary(pca_2014$model)
+#Importance of components:
+#                             Comp.1       Comp.2       Comp.3       Comp.4 ....
+#Standard deviation     7670.7027040 3520.1605260 1.898809e+03 4.330130e+02
+#Proportion of Variance    0.7833149    0.1649647 4.799861e-02 2.496135e-03
+#Cumulative Proportion     0.7833149    0.9482796 9.962782e-01 9.987744e-01
+
+
+set.seed(30)
+pca12_2014 <- pca_2014$map$PC1+pca_2014$map$PC2 # c1 + c2 = 94% of variability
+class2014 <- unsuperClass(pca12, nClasses=7)
+# plot(class2014$map)
+
+e <- drawExtent(show=TRUE, col="red") #estensione area di Rosignano Marittimo (LI), oggetto Extent
+#> e
+#class      : Extent
+#xmin       : 611550.9
+#xmax       : 623497.3
+#ymin       : 4804117
+#ymax       : 4809851
+
+class2014_crop <- crop(class2014$map,e)
 
